974636a390529eb3df31b322bfe69efc505ced48
oRenette <owen.renette@gmail.com>
2023-09-15 21:38:53 -0400

    Main method now creates an AddressBook and a buddy and adds it then removes the buddy from it.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -46,6 +46,9 @@ public class AddressBook {
         return false;
     }
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook  = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
